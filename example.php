72b4849541e9e1f3f8e239ed5c45f5f25007ff50
FederatedStaffing <unixolutions@gmail.com>
2017-04-07 14:06:39 -0400

    Update example.php

--- example.php
+++ example.php
@@ -1,1 +1,14 @@
 
+$process = new Process();
+
+if ($process->create()) {
+    echo "Parent...waiting\n";
+    $process->wait();
+    echo "Parent done\n";
+
+} else {
+    echo "Child....sleeping\n";
+    sleep(2);
+    echo "Child Exiting\n";
+    exit;
+}
